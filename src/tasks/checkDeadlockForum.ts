a491b86f4d65953f94f2dd0d3c185468936ea647
yakasov <yakasovian@gmail.com>
2026-06-01 14:10:12 +0100

    Fix checkDeadlockForum better

--- src/tasks/checkDeadlockForum.ts
+++ src/tasks/checkDeadlockForum.ts
@@ -3,6 +3,8 @@ import { URL_DEADLOCK_FORUM, URL_DEADLOCK_YOSHI } from "../consts/constants";
 import { BotContext } from "../types/BotContext";
 import { Guild, TextChannel } from "discord.js";
 
+type nHTMLElement = HTMLElement | null | undefined;
+
 let lastUrl: string | null = null;
 
 export async function checkDeadlockForum(context: BotContext): Promise<void> {
@@ -10,14 +12,14 @@ export async function checkDeadlockForum(context: BotContext): Promise<void> {
     (response: Response) => response.text()
   );
   const parsedHTML: HTMLElement = HTMLParser(profileText);
-  const lastPost: HTMLElement | null = parsedHTML.querySelector(".contentRow");
+  const lastPost: nHTMLElement = parsedHTML.querySelector(".contentRow");
 
   if (!lastPost) {
     console.warn("Last Deadlock fetch was seemingly unsuccessful!");
     return;
   }
 
-  const aTag: HTMLElement | null = lastPost.querySelector("h3 > a");
+  const aTag: nHTMLElement = lastPost.querySelector("h3 > a");
 
   if (!aTag) return;
 
@@ -27,20 +29,30 @@ export async function checkDeadlockForum(context: BotContext): Promise<void> {
   if (!aTag.innerText.includes("Update")) return;
 
   if (lastUrl !== href) {
-    if (!lastUrl) {
-      lastUrl = href;
-      return;
-    }
-
     const postText: string = await fetch(URL_DEADLOCK_FORUM + href).then(
       (response: Response) => response.text()
     );
     const parsedPostText: HTMLElement = HTMLParser(postText);
-    const lastUpdate: HTMLElement | null = parsedPostText.querySelector(
+    const lastUpdate: nHTMLElement = parsedPostText.querySelector(
       `article[data-content=${postKey}]`
     );
-    const lastUpdateContent: string | undefined =
-      lastUpdate?.querySelector(".bbWrapper")?.innerText;
+
+    const lastUpdateWrapper: nHTMLElement =
+      lastUpdate?.querySelector(".bbWrapper");
+
+    let lastUpdateContent;
+    const codeBlock: nHTMLElement =
+      lastUpdateWrapper?.querySelector(".bbCodeBlock");
+    if (codeBlock) {
+      const url: string = codeBlock.attributes["data-url"];
+      const contentSnippet: string | undefined = codeBlock
+        ?.querySelector("contentRow-snippet")
+        ?.innerText?.split(" - ")
+        .join("\n- ");
+      lastUpdateContent = `${contentSnippet}\n\n${url}`;
+    } else {
+      lastUpdateContent = lastUpdateWrapper?.innerText;
+    }
 
     if (!lastUpdateContent) return;
 
@@ -66,4 +78,6 @@ export async function checkDeadlockForum(context: BotContext): Promise<void> {
 
     await deadlockChannel.send(`# NEW YOSHI POST\n${lastUpdateContent}`);
   }
+
+  lastUrl = href;
 }
