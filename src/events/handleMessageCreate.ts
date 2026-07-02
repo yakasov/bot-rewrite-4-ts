9d4e3fe01999f429fbf98e6e3440ef4e000a87a4
James McDougall <yakasovian@gmail.com>
2026-07-02 10:06:12 +0100

    Add auto-translate for embeds

--- src/events/handleMessageCreate.ts
+++ src/events/handleMessageCreate.ts
@@ -5,12 +5,28 @@ import { addToStats } from "../stats/statsHelpers";
 import { checkMessageInvoke } from "../response/checkMessageInvoke";
 import { goodreadsInvoke } from "../books/goodreads/invoke";
 import { movieInvoke } from "../movies/invoke";
+import tr from "googletrans";
+import cld from "cld";
 
 export async function handleMessageCreate(
   message: Message,
   context: BotContext
 ): Promise<void> {
-  if (message.author.bot || !message.guild) return;
+  if (message.author.bot) {
+    if (message.embeds && message.embeds.length > 0 && message.embeds[0].data && message.embeds[0].data.description) {
+      const detection = await cld.detect(message.embeds[0].data.description);
+
+      if (detection.languages[0].code != "en") {
+        const translation = await tr(message.embeds[0].data.description);
+
+        await message.reply(translation.text.split("**[💬]")[0]);
+      }
+    }
+
+    return;
+  }
+
+  if (!message.guild) return;
 
   await scryfallInvoke(message);
   await goodreadsInvoke(message);
